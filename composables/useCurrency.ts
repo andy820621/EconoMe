9126510fd235edcdebde55c9de4394ce91b5cdee
BarZ <andy820621@gmail.com>
2023-12-23 23:31:52 +0800

    add: install vueUse. update: optimize useCurrency.

--- composables/useCurrency.ts
+++ composables/useCurrency.ts
@@ -1,16 +1,20 @@
-export const useCurrency = (amount: number | Ref<number | null> | null) => {
+export const useCurrency = (
+	amount: MaybeRef<number | null> = 0,
+	locale: string = "en-US",
+	currency: string = "USD"
+) => {
 	const formatCurrency = (value: number) =>
-		new Intl.NumberFormat("en-US", {
+		new Intl.NumberFormat(locale, {
 			style: "currency",
-			currency: "USD",
+			currency,
 		}).format(value);
 
-	const currency = computed(() => {
-		const value = isRef(amount) ? amount.value : amount;
-		return value === null ? null : formatCurrency(value);
+	const currencyValue = computed(() => {
+		const resolvedAmount = isRef(amount) ? amount.value : amount;
+		return resolvedAmount === null ? null : formatCurrency(resolvedAmount);
 	});
 
 	return {
-		currency,
+		currency: currencyValue,
 	};
 };
