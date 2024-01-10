dc41ed8d00b9232bca6498caaff1ea2f1eccc0f8
BarZ <andy820621@gmail.com>
2024-01-11 00:42:34 +0800

    feat: seed some fake data to supabase.

--- seed.mjs
+++ seed.mjs
@@ -2,70 +2,80 @@ import { createClient } from '@supabase/supabase-js'
 import { faker } from '@faker-js/faker'
 import 'dotenv/config'
 
+const SUPABASE_API_SERVICE_ROLE = "" //! Replace with your Supabase API service role, but don't forget to make sure not to show it in public. (ex: delete this value or add this file to .gitignore before push to GitHub)
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY, {
+  // process.env.SUPABASE_KEY, {
+  SUPABASE_API_SERVICE_ROLE, {
   auth: { persistSession: false }
 })
 const categories = ['Food', 'Housing', 'Car', 'Entertainment']
 
+const { data: { users }, error } = await supabase.auth.admin.listUsers()
+const userIds = users.map(user => user.id)
+
 async function seedTransactions() {
   // ! Delete existing data
-  // const { error: deleteError } = await supabase.from('transactions')
-  //   .delete().gte('id', 0)
+  const { error: deleteError } = await supabase.from('transactions')
+    .delete().gte('id', 0)
 
-  // if (deleteError) {
-  //   console.error('Error deleting existing data:', deleteError)
-  //   return
-  // }
+  if (deleteError) {
+    console.error('Error deleting existing data:', deleteError)
+    return
+  }
 
   let transactions = []
 
-  for (let year = new Date().getFullYear(); year > new Date().getFullYear() - 2; year--) {
-    for (let i = 0; i < 10; i++) {
-      const date = new Date(
-        year,
-        faker.number.int({ min: 0, max: 11 }),
-        faker.number.int({ min: 1, max: 28 })
-      )
-
-      let type, category
-      const typeBias = Math.random()
-
-      if (typeBias < 0.85) {
-        type = 'Expense'
-        category = faker.helpers.arrayElement(categories) // Category only for 'Expense'
-      } else if (typeBias < 0.95) {
-        type = 'Income'
-      } else {
-        type = faker.helpers.arrayElement(['Saving', 'Investment'])
+  for (const user of userIds) {
+    for (let year = new Date().getFullYear(); year > new Date().getFullYear() - 2; year--) {
+      for (let i = 0; i < 10; i++) {
+        const date = new Date(
+          year,
+          faker.number.int({ min: 0, max: 11 }),
+          faker.number.int({ min: 1, max: 28 })
+        )
+  
+        let type, category
+        const typeBias = Math.random()
+  
+        if (typeBias < 0.85) {
+          type = 'Expense'
+          category = faker.helpers.arrayElement(categories) // Category only for 'Expense'
+        } else if (typeBias < 0.95) {
+          type = 'Income'
+        } else {
+          type = faker.helpers.arrayElement(['Saving', 'Investment'])
+        }
+  
+        let amount
+        switch (type) {
+          case 'Income':
+            amount = faker.number.int({ min: 2000, max: 5000 })
+            break
+          case 'Expense':
+            amount = faker.number.int({ min: 100, max: 1000 })
+            break
+          case 'Saving':
+          case 'Investment':
+            amount = faker.number.int({ min: 5000, max: 10000 })
+            break
+          default:
+            amount = 0
+        }
+  
+        transactions.push({
+          created_at: date,
+          amount,
+          type,
+          description: faker.lorem.sentence(),
+          category: type === 'Expense' ? category : null, // Category only for 'Expense'
+          user_id: user
+        })
       }
-
-      let amount
-      switch (type) {
-        case 'Income':
-          amount = faker.number.int({ min: 2000, max: 5000 })
-          break
-        case 'Expense':
-          amount = faker.number.int({ min: 100, max: 1000 })
-          break
-        case 'Saving':
-        case 'Investment':
-          amount = faker.number.int({ min: 5000, max: 10000 })
-          break
-        default:
-          amount = 0
-      }
-
-      transactions.push({
-        created_at: date,
-        amount,
-        type,
-        description: faker.lorem.sentence(),
-        category: type === 'Expense' ? category : null // Category only for 'Expense'
-      })
     }
   }
+
 
   const { error: insertError } = await supabase.from('transactions')
     .upsert(transactions)
