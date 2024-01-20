c4e6c50c681a782b1daa4716ac71e0bb7ac82b0f
BarZ <andy820621@gmail.com>
2024-01-20 22:23:55 +0800

    feat: Create settings pages that use Nested routes and Vertical Navigation

---
+++ pages/settings.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="grid grid-cols-6 gap-8">
+		<UVerticalNavigation
+			:links="links"
+			class="col-span-2"
+		></UVerticalNavigation>
+		<div class="col-span-4">
+			<NuxtPage />
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const links = [
+	{
+		label: "Profile",
+		to: "/settings/profile",
+	},
+	{
+		label: "Avatar",
+		to: "/settings/avatar",
+	},
+	{
+		label: "Settings",
+		to: "/settings/settings",
+	},
+];
+</script>
+
+<style scoped lang="scss"></style>
---
+++ pages/settings/avatar.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>Avatar</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
