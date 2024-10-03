4c9073a7c087e1b447af1af29b4ef95875532b1c
BarZ <andy820621@gmail.com>
2024-10-04 07:29:54 +0800

    chore: update deps.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@nuxt/ui", "@nuxtjs/supabase"],
+	// TODO: Missing supabase url, supabase anon key
 	supabase: {
 		redirect: true,
 	},
